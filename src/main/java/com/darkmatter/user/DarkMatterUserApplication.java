ccf6f736195c9b2dc7914f7b358db912cd80710c
Luis Vidal <lvidaldev@gmail.com>
2022-05-24 00:08:34 -0500

    se comienza..

---
+++ src/main/java/com/darkmatter/user/DarkMatterUserApplication.java
@@ -0,0 +1,13 @@
+package com.darkmatter.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkMatterUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarkMatterUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/darkmatter/user/DarkMatterUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkmatter.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkMatterUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
